edce9ccff4db3810b3430d0914966fb0949d3b35
axel-hk <alexannder99@mail.ru>
2021-10-19 23:02:03 +0300

    Исправлена область видимости бинов

--- spring-bean-scope/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-bean-scope/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -1,8 +1,20 @@
 package ru.sber.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import ru.sber.services.FirstService
+import ru.sber.services.SecondService
 
 @Configuration
 @ComponentScan("ru.sber.services")
-class ServicesConfig
+class ServicesConfig{
+    @Bean
+    fun single(): FirstService{
+        return FirstService()
+    }
+    @Bean
+    fun prototype(): SecondService{
+        return SecondService()
+    }
+}
